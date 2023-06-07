44dde46dd2173f4bfa37097d17443de398ab81d9
noprizal <noprizal@voltras.co.id>
2023-06-07 11:13:04 +0700

    add caching with redis

--- src/main/java/com/voltras/simplecrudjavaspringmaven/controller/TutorialController.java
+++ src/main/java/com/voltras/simplecrudjavaspringmaven/controller/TutorialController.java
@@ -1,12 +1,8 @@
 package com.voltras.simplecrudjavaspringmaven.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.voltras.simplecrudjavaspringmaven.model.Tutorial;
 import com.voltras.simplecrudjavaspringmaven.repository.TutorialRepository;
+import com.voltras.simplecrudjavaspringmaven.service.TutorialService;
 
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
@@ -29,102 +26,45 @@ public class TutorialController {
 	@Autowired
 	TutorialRepository tutorialRepository;
 
+	@Autowired
+	TutorialService tutorialService;
+
+	// with cache
 	@GetMapping("/tutorials")
-	public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
-		try {
-			List<Tutorial> tutorials = new ArrayList<Tutorial>();
-
-			if (title == null)
-				tutorialRepository.findAll().forEach(tutorials::add);
-			else
-				tutorialRepository.findByTitleContaining(title).forEach(tutorials::add);
-
-			if (tutorials.isEmpty()) {
-				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-			}
-
-			return new ResponseEntity<>(tutorials, HttpStatus.OK);
-		} catch (Exception e) {
-			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+	public List<Tutorial> getAllTutorials(@RequestParam(required = false) String title) {
+		return tutorialService.getAllTutorialsService(title);
 	}
-
+	// with cache
 	@GetMapping("/tutorials/{id}")
-	public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") long id) {
-		Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
-
-		if (tutorialData.isPresent()) {
-			return new ResponseEntity<>(tutorialData.get(), HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
+	public Tutorial getTutorialById(@PathVariable("id") long id) {
+		return tutorialService.getTutorialByIdService(id);
 	}
-
 	@PostMapping("/tutorials")
-	public ResponseEntity<Tutorial> createTutorial(@RequestBody Tutorial tutorial) {
-		try {
-			Tutorial _tutorial = tutorialRepository
-					.save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(), tutorial.isPublished()));
-			return new ResponseEntity<>(_tutorial, HttpStatus.CREATED);
-		} catch (Exception e) {
-			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+	public Tutorial createTutorial(@RequestBody Tutorial tutorial) {
+		return tutorialService.createTutorialService(tutorial);
 	}
 
+	// with cache
 	@PutMapping("/tutorials/{id}")
-	public ResponseEntity<Tutorial> updateTutorial(@PathVariable("id") long id, @RequestBody Tutorial tutorial) {
-		Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
-
-		if (tutorialData.isPresent()) {
-			Tutorial _tutorial = tutorialData.get();
-			_tutorial.setTitle(tutorial.getTitle());
-			_tutorial.setDescription(tutorial.getDescription());
-			_tutorial.setPublished(tutorial.isPublished());
-			return new ResponseEntity<>(tutorialRepository.save(_tutorial), HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
+	public Tutorial updateTutorial(@PathVariable("id") long id, @RequestBody Tutorial tutorial) {
+		return tutorialService.updateTutorialService(id, tutorial);
 	}
 
+	// with cache
 	@DeleteMapping("/tutorials/{id}")
-	public ResponseEntity<List<Tutorial>> deleteTutorial(@PathVariable("id") long id) {
-		try {
-			tutorialRepository.deleteById(id);
-			
-			//edited
-			List<Tutorial> tutorials = new ArrayList<Tutorial>();
-			tutorialRepository.findAll().forEach(tutorials::add);
-			
-			return new ResponseEntity<>(tutorials, HttpStatus.OK);
-//			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-		} catch (Exception e) {
-			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+	public List<Tutorial> deleteTutorial(@PathVariable("id") long id) {
+		return tutorialService.deleteTutorialService(id);
 	}
 
+	// with cache
 	@DeleteMapping("/tutorials")
-	public ResponseEntity<HttpStatus> deleteAllTutorials() {
-		try {
-			tutorialRepository.deleteAll();
-			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-		} catch (Exception e) {
-			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-
+	public void deleteAllTutorials() {
+		tutorialService.deleteAllTutorialsService();
 	}
 
+	// with cache
 	@GetMapping("/tutorials/published")
-	public ResponseEntity<List<Tutorial>> findByPublished() {
-		try {
-			List<Tutorial> tutorials = tutorialRepository.findByPublished(true);
-
-			if (tutorials.isEmpty()) {
-				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-			}
-			return new ResponseEntity<>(tutorials, HttpStatus.OK);
-		} catch (Exception e) {
-			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+	public List<Tutorial> findByPublished() {
+		return tutorialService.findByPublished();
 	}
-
 }
---
+++ src/main/java/com/voltras/simplecrudjavaspringmaven/service/TutorialService.java
@@ -0,0 +1,107 @@
+package com.voltras.simplecrudjavaspringmaven.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import com.voltras.simplecrudjavaspringmaven.model.Tutorial;
+import com.voltras.simplecrudjavaspringmaven.repository.TutorialRepository;
+
+@Service
+public class TutorialService {
+	@Autowired
+	TutorialRepository tutorialRepository;
+
+	@Cacheable("tutorials")
+	public List<Tutorial> getAllTutorialsService(String title) {
+		doLongRunningTask();
+
+		List<Tutorial> tutorials = new ArrayList<Tutorial>();
+
+		if (title == null)
+			tutorialRepository.findAll().forEach(tutorials::add);
+		else
+			tutorialRepository.findByTitleContaining(title).forEach(tutorials::add);
+
+		if (tutorials.isEmpty()) {
+			return null;
+		}
+
+		return tutorials;
+	}
+
+	@Cacheable("tutorial")
+	public Tutorial getTutorialByIdService(long id) {
+		doLongRunningTask();
+
+		Optional<Tutorial> tutorial = tutorialRepository.findById(id);
+
+		if (tutorial.isPresent()) {
+			return tutorial.get();
+		} else {
+			return null;
+		}
+	}
+
+	public Tutorial createTutorialService(Tutorial tutorial) {
+		Tutorial _tutorial = tutorialRepository
+				.save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(), tutorial.isPublished()));
+		return _tutorial;
+	}
+
+	@CacheEvict(value = "tutorials", allEntries = true)
+	public Tutorial updateTutorialService(long id, Tutorial tutorial) {
+		Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
+
+		if (tutorialData.isPresent()) {
+			Tutorial _tutorial = tutorialData.get();
+			_tutorial.setTitle(tutorial.getTitle());
+			_tutorial.setDescription(tutorial.getDescription());
+			_tutorial.setPublished(tutorial.isPublished());
+			return tutorialRepository.save(_tutorial);
+		} else {
+			return null;
+		}
+	}
+
+	@CacheEvict(value = "tutorials", allEntries = true)
+	public List<Tutorial> deleteTutorialService(long id) {
+		tutorialRepository.deleteById(id);
+
+		List<Tutorial> tutorials = new ArrayList<Tutorial>();
+
+		tutorialRepository.findAll().forEach(tutorials::add);
+
+		return tutorials;
+	}
+
+	@CacheEvict(value = { "tutorials", "tutorial", "tutorials_published", }, allEntries = true)
+	public void deleteAllTutorialsService() {
+		tutorialRepository.deleteAll();
+	}
+
+	@Cacheable("tutorials_published")
+	public List<Tutorial> findByPublished() {
+		doLongRunningTask();
+
+		List<Tutorial> tutorials = tutorialRepository.findByPublished(true);
+
+		if (tutorials.isEmpty()) {
+			return null;
+		}
+		return tutorials;
+	}
+
+	private void doLongRunningTask() {
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
